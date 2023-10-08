92cac972aa334dd1c8639e0dc415aeaa922566af
Ameni-boukottaya14 <ameni.boukattaya@esprit.tngit config --global user.email ameni.boukattaya@esprit.tn>
2023-10-08 18:13:51 +0100

    first commit

---
+++ PrositZoo/src/tn/esprit/gestionzoo/entities/Animal.java
@@ -0,0 +1,66 @@
+package tn.esprit.gestionzoo.entities;
+public class Animal {
+
+    String family, name;
+    int age;
+    boolean isMammal;
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age>0){
+            this.age = age;
+        }
+        else{
+            System.out.println("the age of the animal must be positive");
+        }
+
+    }
+
+    public boolean isMammal() {
+        return isMammal;
+    }
+
+    public void setMammal(boolean mammal) {
+        isMammal = mammal;
+    }
+
+    public Animal() {
+
+    }
+
+    public Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        this.name = name;
+        setAge(age);
+        this.isMammal = isMammal;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "family='" + family + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isMammal=" + isMammal +
+                '}';
+    }
+}
